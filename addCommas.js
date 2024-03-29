6ed2a8ab11461d393e345e64a081ce3c842f5743
Oliver <oliver@MSI>
2024-03-29 12:27:49 -0700

    finished addCommas function

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,21 @@
-function addCommas() {}
+function addCommas(val) {
+    const str = val.toString();
+    const strArr = str.split('').reverse();
+    let count = 1;
+    let result = []; 
+    for (let x of strArr){
+      if (count % 4 !== 0){
+        result.unshift(x);
+      } else {
+        result.unshift(",");
+        result.unshift(x);
+        count++;
+      }
+      count++;
+    }
+    const final = result.join("");
+    return final;
+}
+
 
 module.exports = addCommas;
