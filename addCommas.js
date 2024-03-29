e74430577107c6ff124cfcaad6c43cb59ce18e85
Oliver <oliver@MSI>
2024-03-29 13:44:13 -0700

    accounted for decimals and added tests

--- addCommas.js
+++ addCommas.js
@@ -1,28 +1,57 @@
 function addCommas(val) {
-    const str = val.toString();
-    let neg;
-    if (str[0] === "-"){
-        neg = true;
-    } str = str.replace("-", "");
-    const strArr = str.split('').reverse();
-    let count = 1;
-    let result = []; 
-    for (let x of strArr){
-      if (count % 4 !== 0){
-        result.unshift(x);
-      } else {
-        result.unshift(",");
-        result.unshift(x);
-        count++;
-      }
-      count++;
-    }
-    neg ? result.unshift("-") : result;
-    const final = result.join("");
-    console.log(final)
-    console.log(typeof(final))
-    return final;
-}
+    const str = Math.abs(val).toString();
+    let result = [];
+    let commas = 0;
+    const index = str.indexOf(".");
 
+    if (index !== -1) {
+        const integerPart = str.substring(0, index);
+        const decimalPart = str.substring(index + 1);
+        
+        const integerArr = integerPart.split('').reverse();
+        let count = 1;
+        for (let x of integerArr) {
+            if (count % 4 !== 0) {
+                result.unshift(x);
+            } else {
+                result.unshift(",");
+                result.unshift(x);
+                count++;
+                commas++;
+            }
+            count++;
+        }
+        
+        result.push(".");
+        
+        const decimalArr = decimalPart.split('');
+        for (let x of decimalArr) {
+            result.push(x);
+        }
+    } else {
+        const strArr = str.split('').reverse();
+        let count = 1;
+        for (let x of strArr) {
+            if (count % 4 !== 0) {
+                result.unshift(x);
+            } else {
+                result.unshift(",");
+                result.unshift(x);
+                count++;
+                commas++;
+            }
+            count++;
+        }
+    }
+
+    if (val < 0) {
+        result.unshift("-");
+    }
+
+    const final = result.join("");
+    console.log(final);
+    console.log(typeof(final));
+    return final;        
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -5,3 +5,31 @@ describe("#addCommas", () => {
     expect(typeof addCommas).toBe("function");
   });
 });
+
+describe("test for positive integers", () => {
+  it("should return a string", () => {
+    expect(typeof addCommas(1234)).toBe("string");
+    expect(typeof addCommas(1000000)).toBe("string");
+    expect(typeof addCommas(9876543210)).toBe("string");
+    expect(typeof addCommas(6)).toBe("string");
+  });
+  it("should have a comma in the right space", () => {
+    expect(addCommas(1234).indexOf(",")).toBe(1);
+    expect(addCommas(1000000).indexOf(",")).toBe(1);
+    expect(addCommas(1000000).indexOf(",", 2)).toBe(5);
+    expect(addCommas(9876543210).indexOf(",")).toBe(1);
+    expect(addCommas(9876543210).indexOf(",", 2)).toBe(5);
+    expect(addCommas(9876543210).indexOf(",", 6)).toBe(9);
+    expect(addCommas(6).indexOf(",")).toBe(-1);
+  })
+});
+
+describe("test for negative integers", () => {
+  it("should return a string", () => {
+    expect(typeof addCommas(-10)).toBe("string");
+  });
+  it("should include a negative sign", () => {
+    expect(addCommas(-10).includes("-")).toBe(true);
+    expect(addCommas(10).includes("-")).toBe(false);
+  })
+});
